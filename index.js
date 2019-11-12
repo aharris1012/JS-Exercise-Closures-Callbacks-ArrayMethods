d4c1bdb9c1e7730a7f7328456129fdebae8c8f4e
Andrea Quintana <andreaquintana@andreas-mbp.lan>
2019-11-12 15:40:54 -0600

    challenege3

--- index.js
+++ index.js
@@ -91,7 +91,8 @@ function processLastItem(stringList, callback) {
  * [2] Invoking `processSum` passing `[]` and `(num) => num + 1000`,
  * should return 1000.
 */
-function processSum(/* CODE HERE */) {
+function processSum(numberList, callback) {
+  return callback (numberList.reduce);
   /* CODE HERE */
 }
 
